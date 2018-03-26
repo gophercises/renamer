3c7b5e80a0a044b8a681ead29b55defe97de9ca0
Jon Calhoun <joncalhoun@gmail.com>
2018-03-26 18:32:48 -0400

    wrote a match function

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fileName := "birthday_001.txt"
+	// => Birthday - 1 of 4.txt
+	newName, err := match(fileName, 4)
+	if err != nil {
+		fmt.Println("no match")
+		os.Exit(1)
+	}
+	fmt.Println(newName)
+}
+
+// match returns the new file name, or an error if the file name
+// didn't match our pattern.
+func match(fileName string, total int) (string, error) {
+	// "birthday", "001", "txt"
+	pieces := strings.Split(fileName, ".")
+	ext := pieces[len(pieces)-1]
+	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
+	pieces = strings.Split(tmp, "_")
+	name := strings.Join(pieces[0:len(pieces)-1], "_")
+	number, err := strconv.Atoi(pieces[len(pieces)-1])
+	if err != nil {
+		return "", fmt.Errorf("%s didn't match our pattern", fileName)
+	}
+	// Birthday - 1.txt
+	return fmt.Sprintf("%s - %d of %d.%s", strings.Title(name), number, total, ext), nil
+}
